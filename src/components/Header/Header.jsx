eac6e4e809c9c59b2463f2d68b7ef5ce6480a44d
irinaugrak <irina.ugrak@gmail.com>
2022-11-19 10:51:45 +0300

    add header test

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,1 +1,11 @@
-import styles from './Header.scss';
+import styles from './Header.css';
+
+function Header() {
+    return (
+        <div className={styles.header}>
+            <p>Здесь будет хедер</p>
+        </div>
+    )
+}
+
+export default Header;
