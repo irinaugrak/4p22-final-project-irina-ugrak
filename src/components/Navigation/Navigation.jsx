fa2ba421ef5b560eda850d9e582a7f494cdfa4d3
irinaugrak <irina.ugrak@gmail.com>
2022-12-03 20:00:55 +0300

    navigation on categories

--- src/components/Navigation/Navigation.jsx
+++ src/components/Navigation/Navigation.jsx
@@ -1,20 +1,33 @@
 import './Navigation.scss';
 import { Link } from "react-router-dom";
+import { useParams } from "react-router-dom";
 
 function Navigation({type}) {
+    const { category } = useParams();
+    console.log(category);
     return (
         <nav className={`navigation navigation-${type}`}>
             <ul className={`navigation__list navigation-${type}__list`}>
                 <Link to={"/sofas-and-armchairs"} className="navigation__link">
-                    <li className={`navigation__item navigation-${type}__item navigation-${type}__item_current`}>
+                    <li
+                        className={`navigation__item navigation-${type}__item ${
+                            category === "sofas-and-armchairs" ? `navigation-${type}__item_current` : ""
+                        }`}>
                         Диваны и кресла
                     </li>
                 </Link>
                 <Link to={"/tables-and-chairs"} className="navigation__link">
-                    <li className={`navigation__item navigation-${type}__item`}>Столы и стулья</li>
+                    <li
+                        className={`navigation__item navigation-${type}__item ${
+                            category === "tables-and-chairs" ? `navigation-${type}__item_current` : ""
+                        }`}>
+                        Столы и стулья
+                    </li>
                 </Link>
 
-                <li className={`navigation__item navigation-${type}__item`}>Шкафы и стеллажи</li>
+                <li className={`navigation__item navigation-${type}__item`}>
+                    Шкафы и стеллажи
+                </li>
                 <li className={`navigation__item navigation-${type}__item`}>Кровати и матрасы</li>
                 <li className={`navigation__item navigation-${type}__item`}>Комоды и тумбы</li>
             </ul>
